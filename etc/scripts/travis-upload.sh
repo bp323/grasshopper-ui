ed96b813e5ea3bd21470f9769d1827d6431048e2
Bert Pareyn <pareyn.bert@gmail.com>
2015-03-18 14:09:56 +0000

    Debug artifact upload on Travis

--- etc/scripts/travis-upload.sh
+++ etc/scripts/travis-upload.sh
@@ -10,12 +10,18 @@
 
 EXPECTED_REPOSITORY="CUL-DigitalServices/grasshoppper-ui"
 EXPECTED_BRANCH="master"
-EXPECTED_PULL_REQUEST=false
+EXPECTED_PULL_REQUEST=true
 
 function package_and_upload {
-    rm -rf target
     bin/package -su --upload-bucket=grasshopper-ui-releases --upload-region=eu-west-1
 }
+
+echo '$TRAVIS_REPO_SLUG:' $TRAVIS_REPO_SLUG
+echo '$EXPECTED_REPOSITORY:' $EXPECTED_REPOSITORY
+echo '$TRAVIS_BRANCH:' $TRAVIS_BRANCH
+echo '$EXPECTED_BRANCH:' $EXPECTED_BRANCH
+echo '$TRAVIS_PULL_REQUEST:' $TRAVIS_PULL_REQUEST
+echo '$EXPECTED_PULL_REQUEST:' $EXPECTED_PULL_REQUEST
 
 if [[ "$TRAVIS_REPO_SLUG" == "$EXPECTED_REPOSITORY" && "$TRAVIS_BRANCH" == "$EXPECTED_BRANCH" && "$TRAVIS_PULL_REQUEST" == "$EXPECTED_PULL_REQUEST" ]]; then
     package_and_upload
